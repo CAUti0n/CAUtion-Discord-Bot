d3a82fa8db6221bbb285aa37e5d729d7f4ec1feb
1unaram <haram620@icloud.com>
2023-04-13 16:59:37 +0900

    Change notion.py

--- notion.py
+++ notion.py
@@ -1,4 +1,3 @@
-import aiohttp
 from itemProperties import ip
 import dotenv
 import os
@@ -12,45 +11,8 @@ token = str(os.getenv("NOTION_TOKEN"))
 database_id = str(os.getenv("DATABASE_ID"))
 
 
-# # Get Item List
-# def get_item_list():
-#     url = f'https://api.notion.com/v1/databases/{database_id}/query'
-#     headers = {
-#         "Authorization": f"Bearer {token}",
-#         "accept": "application/json",
-#         "Notion-Version": "2022-06-28",
-#         "content-type": "application/json"
-#     }
-#     payload = {
-#         "page_size": 100,
-#         "sorts": [
-#             {
-#                 "property": "Name",
-#                 "direction": "ascending"
-#             }
-#         ]
-#     }
-
-#     response = requests.post(url, json=payload, headers=headers)
-#     objects = json.loads(response.text)['results']
-
-#     items = []
-#     for o in objects:
-#         item = {
-#             'id': o['properties']['ID']['number'],
-#             'name': o['properties']['Name']['title'][0]['plain_text'],
-#             'type': o['properties']['Type']['select']['name'].strip(),
-#             'status': o['properties']['Status']['status']['name'],
-#         }
-#         items.append(item)
-
-#     # 출력
-#     # print(json.dumps(json.loads(response.text), indent=2, ensure_ascii=False))
-
-#     return items
-
-
-async def get_item_list():
+# Get Item List
+def get_item_list():
     url = f'https://api.notion.com/v1/databases/{database_id}/query'
     headers = {
         "Authorization": f"Bearer {token}",
@@ -67,9 +29,9 @@ async def get_item_list():
             }
         ]
     }
-    async with aiohttp.ClientSession() as session:
-        async with session.post(url, json=payload, headers=headers) as response:
-            objects = (await response.json())['results']
+
+    response = requests.post(url, json=payload, headers=headers)
+    objects = json.loads(response.text)['results']
 
     items = []
     for o in objects:
@@ -80,6 +42,9 @@ async def get_item_list():
             'status': o['properties']['Status']['status']['name'],
         }
         items.append(item)
+
+    # 출력
+    # print(json.dumps(json.loads(response.text), indent=2, ensure_ascii=False))
 
     return items
 
